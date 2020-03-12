8766d20be83ae94ad9999acf64c057736dd8eaf7
Sorrab <ramonpbarros@gmail.com>
2020-03-11 23:21:36 -0700

    fixed mistake w upercase and testing func shuffle

--- script.js
+++ script.js
@@ -44,13 +44,13 @@ function generatePassword() {
   // checl IF answer is true
   if (numbers === true) {
     for (var i = 0; i < newPassword.length; i++) {
-      var optionsOfLettersI = (newPassword[i] === "c") ||
-        (newPassword[i] === "f") ||
-        (newPassword[i] === "j") ||
-        (newPassword[i] === "n") ||
-        (newPassword[i] === "r") ||
-        (newPassword[i] === "u") ||
-        (newPassword[i] === "y");
+      var optionsOfLettersI = (newPassword[i] === "a") ||
+        (newPassword[i] === "c") ||
+        (newPassword[i] === "e") ||
+        (newPassword[i] === "g") ||
+        (newPassword[i] === "i") ||
+        (newPassword[i] === "k") ||
+        (newPassword[i] === "m");
       if (optionsOfLettersI) {
         newPassword.splice(Math.floor(Math.random() * newPassword.length), 1, Math.floor(Math.random() * 10));
       }
@@ -63,14 +63,26 @@ function generatePassword() {
   if (lowerCases === true && upperCases === true) {
     //make some indexes upcase
     for (var j = 0; j < newPassword.length; j++) {
+
       var optionsOfLettersJ = (newPassword[j] === "b") ||
-        (newPassword[j] === "e") ||
-        (newPassword[j] === "i") ||
+        (newPassword[j] === "d") ||
+        (newPassword[j] === "g") ||
+        (newPassword[j] === "j") ||
         (newPassword[j] === "m") ||
+        (newPassword[j] === "p") ||
+        (newPassword[j] === "s");
+
+      var optionsOfLettersJtwo = (newPassword[j] === "v") ||
+        (newPassword[j] === "x") ||
+        (newPassword[j] === "z") ||
+        (newPassword[j] === "k") ||
+        (newPassword[j] === "l") ||
         (newPassword[j] === "q") ||
-        (newPassword[j] === "t") ||
-        (newPassword[j] === "x");
+        (newPassword[j] === "i");
+
       if (typeof newPassword[j] === "string" && optionsOfLettersJ) {
+        newPassword[j] = newPassword[j].toUpperCase();
+      } else if (typeof newPassword[j] === "string" && optionsOfLettersJtwo) {
         newPassword[j] = newPassword[j].toUpperCase();
       }
     }
@@ -91,7 +103,7 @@ function generatePassword() {
       }
     }
   } else {
-  lowerCases === true && upperCases === false
+    lowerCases === true && upperCases === false
     //dont do anything
   }
 
@@ -105,13 +117,43 @@ function generatePassword() {
         (newPassword[m] === "p") ||
         (newPassword[m] === "s") ||
         (newPassword[m] === "w");
+
+      var optionsOfLettersMtwo = (newPassword[m] === "b") ||
+        (newPassword[m] === "e") ||
+        (newPassword[m] === "i") ||
+        (newPassword[m] === "m") ||
+        (newPassword[m] === "q") ||
+        (newPassword[m] === "t") ||
+        (newPassword[m] === "x");
+
+
       if (optionsOfLettersM) {
         newPassword.splice(Math.floor(Math.random() * newPassword.length), 1, specialCharacters[Math.floor(Math.random() * 10)]);
+      } else if (optionsOfLettersMtwo){
+        newPassword.splice(Math.floor(Math.random() * newPassword.length), 1, specialCharacters[Math.floor(Math.random() * 10)]);
       }
-    }
-    console.log(newPassword);
+      }
   }
 
+  function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+  }
+  console.log(newPassword)
+  shuffle(newPassword);
+  console.log(newPassword)
   newPassword = newPassword.join("");
   return newPassword;
 }
